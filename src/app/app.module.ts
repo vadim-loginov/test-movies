11c2435c7c5956dc5a5362a36e20b85685f1c96f
Vadim Loginov <vadim.loginov.mail@gmail.com>
2019-09-26 00:39:57 +0300

    Add github link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material.module';
-import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent
   ],
   imports: [
     BrowserModule,
