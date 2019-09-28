b0a291e39824905df77c79bcb377f39fc6b93207
Vadim Loginov <vadim.loginov.mail@gmail.com>
2019-09-28 12:34:11 +0300

    App shell, basic navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { FavouritesComponent } from './components/favourites/favourites.component';
@@ -8,11 +9,16 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 
 const routes: Routes = [
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/movies',
+  },
+  {
     path: 'movies',
     component: MovieListComponent,
   },
   {
-    path: 'movie/:id',
+    path: 'movies/:id',
     component: MovieDetailsComponent,
   },
   {
@@ -20,18 +26,13 @@ const routes: Routes = [
     component: FavouritesComponent,
   },
   {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/movies',
-  },
-  {
     path: '**',
     component: PageNotFoundComponent,
-  }
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        AngularMaterialModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-movies'`, () => {
+  it(`should have 2 main menu links`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-movies');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-movies app is running!');
+    expect(app.mainMenuLinks.length).toEqual(2);
   });
 });
