6993fc799558ed6ecdbedac895c1dcedc3773188
Vadim Loginov <vadim.loginov.mail@gmail.com>
2019-10-09 00:52:12 +0300

    userTags work

--- src/app/feature-modules/movie-details/services/movie.service.ts
+++ src/app/feature-modules/movie-details/services/movie.service.ts
@@ -35,6 +35,15 @@ export class MovieService {
     return Promise.resolve(this.userTags || []);
   }
 
+  addUserTag(tag) {
+    this.userTags = this.userTags || [];
+
+    if (this.userTags.indexOf(tag) < 0) {
+      this.userTags.push(tag);
+      this.userSettings.userTags = this.userTags;
+    }
+  }
+
   addMovieTag(movieId, tag) {
     this.movieTags = this.movieTags || {};
 
@@ -47,6 +56,7 @@ export class MovieService {
     if (tags.indexOf(tag) < 0) {
       tags.push(tag);
       this.userSettings.movieTags = this.movieTags;
+      this.addUserTag(tag);
     }
   }
 
